34858dbc4a444c2fe7fd4ab228de961289f9222c
LeslieJR <l.reyes.ubiqum@gmail.com>
2023-10-20 00:41:55 +0200

    statistics based on promotions of a specific retailer

--- server/src/controllers/coupons.js
+++ server/src/controllers/coupons.js
@@ -43,3 +43,32 @@ exports.getCouponsByType = async (req, res, next) => {
       res.status(500).json({ status: 'error', message: `An error occurred while retrieving the coupons of type ${type}`, error: error.message });
     }
   };
+
+  exports.getCouponsByRetailer = async (req, res, next) => {
+    try {
+      const { retailer } = req.params;
+      const filteredByRetailer = data.coupons.filter((c) => c.webshop_id === retailer);
+  
+      if (!filteredByRetailer.length) {
+        res.status(400).json({ status: 'error', message: `No coupons found for this retailer (${retailer})` });
+      } else {
+        const uniquePromotionTypes = filteredByRetailer.reduce((types, coupon) => {
+          if (coupon.promotion_type && !types.includes(coupon.promotion_type)) {
+            types.push(coupon.promotion_type);
+          }
+          return types;
+        }, []);
+  
+        const promotionsPerRetailerAndType = uniquePromotionTypes.map((type) => {
+          const couponsPerType = filteredByRetailer.filter((coupon) => coupon.promotion_type === type);
+          const statistics = calculatePromotionStatistics(couponsPerType);
+          return { type_name: type, ...statistics };
+        });
+  
+        res.status(200).json({ status: 'success', promotionsPerRetailerAndType, retailer });
+      }
+  
+    } catch (error) {
+      res.status(500).json({ status: 'error', message: `An error occurred while retrieving the coupons for this retailer (${retailer})`, error: error.message });
+    }
+  };
--- server/src/routes/coupons.js
+++ server/src/routes/coupons.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 
 router.get('/coupons', controller.getAllCoupons); 
 router.get('/couponsByType/:type', controller.getCouponsByType); 
+router.get('/couponsByRetailer/:retailer', controller.getCouponsByRetailer); 
 
 module.exports = router;
