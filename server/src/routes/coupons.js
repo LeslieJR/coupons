0679027ec8276f2e6fd851efc6be3469b9d51129
LeslieJR <l.reyes.ubiqum@gmail.com>
2023-10-20 08:20:14 +0200

    routes updated in server

--- server/src/routes/coupons.js
+++ server/src/routes/coupons.js
@@ -2,7 +2,7 @@ const controller = require('../controllers/coupons');
 const router = require('express').Router();
 
 router.get('/coupons', controller.getAllCoupons); 
-router.get('/couponsByType/:type', controller.getCouponsByType); 
-router.get('/couponsByRetailer/:retailer', controller.getCouponsByRetailer); 
+router.get('/coupons/type/:type', controller.getCouponsByType); 
+router.get('/coupons/retailer/:retailer', controller.getCouponsByRetailer); 
 
 module.exports = router;
